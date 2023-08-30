8a122e87fd488b6bee895eaffa4bc4c3f5675e60
Ishan Nigam <ishannigam@gmail.com>
2023-08-30 11:42:41 +0530

    LC use array as hashmap for O(1) solutions

---
+++ tech-interview-handbook/problems/0442-find-all-duplicates-in-an-array.py
@@ -0,0 +1,25 @@
+"""
+Problem URL: https://leetcode.com/problems/find-all-duplicates-in-an-array
+
+Approach:
+    Use the array indexes as key and the sign of the array element as a val
+    to create a O(1) hashmap.
+TC:
+    Linear pass over array with O(1) ops => O(N).
+Space:
+    answer is in return space, so nothing => O(1).
+"""
+class Solution:
+    def findDuplicates(self, nums):
+
+        N = len(nums)
+        answer = []
+
+        for idx, num in enumerate(nums):
+            if nums[abs(num)-1]<0:
+                answer.append(abs(num))
+            else:
+                nums[abs(num)-1] *= -1
+
+        return answer
+
