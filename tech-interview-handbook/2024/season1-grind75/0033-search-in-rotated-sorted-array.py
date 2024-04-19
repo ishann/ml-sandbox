654c1a3e55bdaab8750c2696eae12bc9d84dd039
Ishan Nigam <ishannigam@gmail.com>
2024-04-19 09:09:57 +0530

    retry a tricky LC medium

--- tech-interview-handbook/2024/season1-grind75/0033-search-in-rotated-sorted-array.py
+++ tech-interview-handbook/2024/season1-grind75/0033-search-in-rotated-sorted-array.py
@@ -11,6 +11,9 @@ Method 1 : NeetCode
     Space:
         l, r => O(1).
 
+    PS: I don't like this solution, because it deviates
+        from several Binary Search norms.
+
 Method 2 : LeetCode Forums
     Approach:
 
@@ -23,6 +26,56 @@ Method 2 : LeetCode Forums
 class Solution:
 
     def search(self, nums: List[int], target: int) -> int:
+
+        l, r = 0, len(nums)
+
+        while l<r:
+
+            mid = (l+r)//2
+            if nums[mid]>nums[0] and target<nums[0]:
+                l=mid+1
+            elif nums[mid]<nums[0] and target>=nums[0]:
+                r=mid
+            elif nums[mid]<target:
+                l=mid+1
+            elif nums[mid]>target:
+                r=mid
+            else:
+                return mid
+
+        return -1
+
+    def search_top_LC_soln(self,
+                           nums: List[int],
+                           target: int) -> int:
+
+        l, r = 0, len(nums)-1
+
+        # Find pivot of rotation.
+        while l<r:
+            mid = (l+r)//2
+            if nums[mid]>nums[r]:
+                l=mid+1
+            else:
+                r=mid
+
+        rot=l
+
+        # Do binary search accounting for pivot of rotation.
+        l, r=0, len(nums)-1
+        while l<=r:
+            mid = (l+r)//2
+            real_mid = (mid+rot)%len(nums)
+            if nums[real_mid]==target:
+                return real_mid
+            if nums[real_mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+
+        return -1
+
+    def search_try1(self, nums: List[int], target: int) -> int:
 
         l, r = 0, len(nums)
 
@@ -41,24 +94,4 @@ class Solution:
 
         return -1
 
-    def search_neetcode(self, nums: List[int], target: int) -> int:
 
-        l,r=0,len(nums)-1
-
-        while l<=r:
-            mid=(l+r)//2
-            if target==nums[mid]:
-                return mid
-
-            if nums[mid]>=nums[l]:
-                if target<nums[l] or target>nums[mid]:
-                    l=mid+1
-                else:
-                    r=mid-1
-            else:
-                if target<nums[mid] or target>nums[r]:
-                    r=mid-1
-                else:
-                    l=mid+1
-
-        return -1
