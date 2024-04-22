1c905cb0cc64d6bb5ad76533270d35352b6d883c
Ishan Nigam <ishannigam@gmail.com>
2024-04-22 09:37:53 +0530

    one easy LC. it is better than no LC.

---
+++ tech-interview-handbook/2024/season1-grind75/0206-reverse-linked-list.py
@@ -0,0 +1,38 @@
+"""
+Approach:
+    Use a temporary variable to exchange pointers.
+    Then, advance curr to curr.next.
+
+TC:
+    Linear pass of LL with constant ops => O(N).
+
+Space:
+    A few temporary variables => O(1).
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def reverseList(self,
+                    head: Optional[ListNode]) \
+                    -> Optional[ListNode]:
+
+        prev = None
+        curr = head
+
+        while curr:
+
+            # Exchange curr and prev using tmp.
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+
+            # Move forward.
+            curr = tmp
+
+        return prev
+
+
